9aa83481f45f87f70b004177d43aff7c082d8cd5
chopragit-source <chopragit@gmail.com>
2026-08-07 15:51:27 +0530

    Update SmoothScroll.tsx

--- src/components/SmoothScroll.tsx
+++ src/components/SmoothScroll.tsx
@@ -8,7 +8,7 @@ export default function SmoothScroll({ children }: { children: React.ReactNode }
 
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 1.2,
+      duration: 1.4,
       easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       touchMultiplier: 2,
     });
